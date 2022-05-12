093d34c9630ac460a2832d130edb62d35d17efce
HiagoMacedo <105092072+HiagoMacedo@users.noreply.github.com>
2022-05-12 13:37:40 -0300

    Update Disciplina.java

--- src/Disciplina.java
+++ src/Disciplina.java
@@ -28,7 +28,7 @@ public class Disciplina {
         }
     }
 
-    private static void exibir(String [] arr, int count) {
+    private static void exibir(String[] arr, int count) {
 
         System.out.println("Possibilidade " + count);
 
