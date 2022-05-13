4b0015d4287fac2ca08cbe80ebc36cf37e13ef0c
HiagoMacedo <105092072+HiagoMacedo@users.noreply.github.com>
2022-05-13 17:38:35 -0300

    Update Fatorial.java

--- src/Fatorial.java
+++ src/Fatorial.java
@@ -3,15 +3,19 @@ public class Fatorial {
     public void solucao() {
         // 1. Programa que imprima o valor fatorial e todas as etapas do seu processo
 
-        int numero = 5;
-        int auxNum;
+        long numero = 15;
+        long auxNum;
+        
+        System.out.println("Fatorial de " + numero);
 
-        for (int i = numero; i > 1; i--) {
+        for (long i = numero; i > 1; i--) {
             auxNum = i - 1;
 
-            System.out.println(numero + " * " + auxNum + " = " + (numero*auxNum));
+            System.out.println(numero + " * " + auxNum + " = " + (numero * auxNum));
 
             numero *= auxNum;
         }
+        
+        System.out.println("Resultado do fatorial: " + numero);
     }
 }
